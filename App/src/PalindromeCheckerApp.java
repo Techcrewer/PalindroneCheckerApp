74973a18790988792db06551c7f385e312ea5376
Surbhi Kaushal <sk7391@srmist.edu.in>
2026-03-03 22:54:42 +0530

    UC4-CharacterArrayBasedPalindromeCheck

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to the palindrone checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialised successfully.");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String input = sc.nextLine();
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+
+        sc.close();
     }
 }
