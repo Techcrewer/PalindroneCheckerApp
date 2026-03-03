0b2b4536570b7af7e4833af4fb2f512fadf6787b
SurbhiKaushal <surbhikaushal8@gmail.com>
2026-03-03 22:42:05 +0530

    UC3: PalindromeCheckUsingStringReverse

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PalindroneCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input Text: ");
+        String text = sc.next();
+        int n= text.length();
+        System.out.println(text);
+        String newText="";
+        for(int i=n-1; i>=0; i--){
+            newText= newText + text.charAt(i);
+        }
+        if(text.equals(newText)){
+            System.out.println("Is it a palindrome? : True");
+        }
+        else{
+            System.out.println("Is it a palindrome? : False");
+        }
+    }
+}
